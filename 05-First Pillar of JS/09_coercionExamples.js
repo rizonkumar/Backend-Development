5dce3fed11438969b971f52d855f00901b08ac57
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-01-10 22:07:49 +0530

    Latest Code

--- 05-First Pillar of JS/09_coercionExamples.js
+++ 05-First Pillar of JS/09_coercionExamples.js
@@ -1,4 +1,4 @@
 console.log(1 < 2 < 3);
-    // (1 < 2) = true -> (true < 3) -> (1 < 3) -> true
+// (1 < 2) = true -> (true < 3) -> (1 < 3) -> true
 console.log(3 > 2 > 1);
-    // (3 > 2) = true -> (true > 1) -> (1 > 1) -> false
+// (3 > 2) = true -> (true > 1) -> (1 > 1) -> false
