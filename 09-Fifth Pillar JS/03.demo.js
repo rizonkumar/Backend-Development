a0b983cd6128ec59c5f2c8fe7ff7b884fe5c8724
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-01-25 08:24:53 +0530

    demo0

--- 09-Fifth Pillar JS/03.demo.js
+++ 09-Fifth Pillar JS/03.demo.js
@@ -8,29 +8,31 @@ function timeConsumingByLoop() {
 
 function timeConsumingByRunTimeFeature0() {
   console.log("Started timer 0");
-  setTimeout(function exec() {
+  setTimeout(function exec0() {
     console.log("Completed the timer0");
     for (let i = 0; i < 1000000; i++) {
       // some task
     }
-  }, 5000); //
+  }, 5000); // 5 sec timer
 }
 function timeConsumingByRunTimeFeature1() {
   console.log("Started timer 1");
-  setTimeout(function exec() {
+  setTimeout(function exec1() {
     console.log("Completed the timer1");
-  }, 0);
+  }, 0); // 0 sec timer
 }
 function timeConsumingByRunTimeFeature2() {
   console.log("Started timer 2");
-  setTimeout(function exec() {
+  setTimeout(function exec2() {
     console.log("Completed the timer2");
-  }, 200);
+  }, 200); // 2ms timer
 }
 console.log("Hi");
 
 timeConsumingByLoop();
-timeConsumingByRunTimeFeature();
+timeConsumingByRunTimeFeature0();
+timeConsumingByRunTimeFeature1();
+timeConsumingByRunTimeFeature2();
 timeConsumingByLoop();
 
 console.log("Bye");
