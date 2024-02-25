99b4fd7006db9e92c3b402d5a02c59ef76caaa6c
rizonkumar <rizon.kumar.rahi@gmail.com>
2024-02-25 10:23:40 +0530

    Creating a server

---
+++ server.py
@@ -0,0 +1,26 @@
+import socket
+import threading
+
+
+def connect_a_client(conn, addr):
+    print("New client has been connnected")
+
+HOST = "localhost"
+
+PORT = 3000
+
+# created a new socket object
+server_socket = socket.socket()
+
+# bind the socket object to the host & port
+server_socket.bind((HOST, PORT))
+
+# start listening for new connection
+server_socket.listen()
+
+print("Server is listening on", HOST, PORT)
+
+while True:
+    conn, addr = server_socket.accept() # it is accepting a new connetion
+    t = threading.Thread(target = connect_a_client, args = (conn, addr)) #it is preparing the thread
+    t.start() # it start running the thread
